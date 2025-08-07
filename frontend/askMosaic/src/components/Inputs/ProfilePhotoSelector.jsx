f3ec096db727e724164d541ffdb742fa5e32f854
tiwariharsh007 <tiwari007harsh@gmail.com>
2025-08-08 00:52:05 +0530

    SignUp UI

---
+++ frontend/askMosaic/src/components/Inputs/ProfilePhotoSelector.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {LuUser, LuUpload, LuTrash} from 'react-icons/lu';
+
+const ProfilePhotoSelector = ({image, setImage, preview, setPreview}) => {
+    
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProfilePhotoSelector
--- frontend/askMosaic/src/pages/Auth/SignUp.jsx
+++ frontend/askMosaic/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
 
-const SignUp = () => {
+const SignUp = ( {setCurrentPage} ) => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  // Handle profile picture change
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      SignUp
+    <div className='w-[80vw] md:w-[33vw] p-7 flex flex-col justify-center'>
+      <h3 className='text-lg font-semibold text-black'>Create an Account</h3>
+      <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+        Join us and start your journey with us.
+      </p>
+
+      <form onSubmit={handleSignUp}>
+        <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+        <div className='grid grid-cols-1 md:grid-cols-1 gap-2'>
+          <Input
+            value={fullName}
+            onChange={({target}) => setFullName(target.value)}
+            label="Full Name"
+            placeholder="Enter your full name"
+            type="text"
+          />
+
+          <Input
+            value={email}
+            onChange={({target}) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+            type="text"
+          />
+
+          <Input
+            value={password}
+            onChange={({target}) => setPassword(target.value)}
+            label="Password"
+            placeholder="Minimum 8 characters"
+            type="password"
+          />
+        </div>
+
+        {error && 
+          <p className='text-red-500 text-xs pb-2.5'>
+            {error}
+          </p>
+        }
+
+        <button type='submit' className='btn-primary'>
+          Sign Up
+        </button>
+
+        <p className='text-[13px] text-slate-800 mt-3'>
+          Already have an account? {" "}
+          <button 
+            className='font-medium text-primary underline cursor-pointer'
+            onClick={() => {
+              setCurrentPage('login');
+            }}
+          >
+            Login
+          </button>
+        </p>
+
+      </form>
     </div>
   )
 }
